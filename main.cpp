daa394e5d20320e5946022cf9e703ba0c55ddb5c
igzi <igor.pavlovic.2001@gmail.com>
2023-08-30 14:38:06 +0200

    Dodato ucitavanje parametara iz komandne linije

--- main.cpp
+++ main.cpp
@@ -5,15 +5,17 @@
 #include <filesystem>
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
 	double bestKnownSol;
 	Problem p;
 
+	int seed = std::stoi(argv[2]);
+
 	std::random_device rd;
 	std::mt19937 generator(rd());
 
-	p = loadData("pmed2.dat", bestKnownSol);
+	p = loadData(argv[1], bestKnownSol);
 
 	generator.seed(1);
 
